e13fcb4973d3839291cfcb94e6365357789bd4d4
Juan Diego Gutierrez Cortez <150155186+1di210299@users.noreply.github.com>
2025-03-04 17:41:28 -0500

    Delete ChatBot_DeepSeek directory

--- ChatBot_DeepSeek/ui/instructions.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from config.settings import WELCOME_MESSAGE, INSTRUCTIONS
-
-def render_instructions():
-    """
-    Display welcome message and instructions for new users
-    """
-    st.info(WELCOME_MESSAGE)
-    st.markdown(INSTRUCTIONS)
